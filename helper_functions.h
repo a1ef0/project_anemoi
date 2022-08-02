04611244d952e08e4aef62022de4ccc1c7dcbe44
a1ef0 <a.buchnev@innopolis.university>
2022-08-02 19:26:52 +0300

    add babystep-giantsep collision algorithm & fix `pow`

--- helper_functions.h
+++ helper_functions.h
@@ -2,7 +2,10 @@
 #define HELPER_FUNCTIONS_H
 
 #include <cln/integer.h>
+#include <cln/real.h>
 #include <set>
+#include <vector>
+#include <map>
 
 namespace anemoi {
 
@@ -31,13 +34,10 @@ cln::cl_I pow(cln::cl_I base, cln::cl_I exp, const cln::cl_I& mod) {
     if (base < 0) {
         throw std::runtime_error("Anemoi error: cannot pow negative number");
     }
-    if (exp == MINUS_ONE && cln::gcd(base, mod) == 1) {
-        exp = mod - 2;
+    if (exp < 0 && cln::gcd(base, mod) == 1) {
+        exp = cln::abs(exp) * (mod - 2);
     } else if (cln::gcd(base, mod) != 1) {
         throw std::runtime_error("Anemoi error: cannot find multiplicative inverse, gcd is not 1");
-    }
-    if (exp < 0) {
-        base = anemoi::pow(base, MINUS_ONE, mod);
     }
     while (exp > 0) {
         if (cln::mod(exp, 2) == 1) {
@@ -70,6 +70,36 @@ cln::cl_I ord(cln::cl_I element, cln::cl_I mod, std::set<cln::cl_I>(*factorize)(
             return exp;
         }
     }
+    return -1;
+}
+
+cln::cl_I shanks_babystep_giantstep(cln::cl_I g, cln::cl_I h, cln::cl_I p) {
+    //Ceiling for computing map values
+    cln::cl_I n = 1 + cln::round1(cln::sqrt(ord(g, p, anemoi::slow_factorize)));
+
+    long long t = cln::cl_I_to_long(n);
+
+    std::map<cln::cl_I, cln::cl_I> lookup;
+
+    //Baby steps
+    for (cln::cl_I i = 0; i < n; ++i) {
+        lookup[anemoi::pow(g, i, p)] = i + 1;
+    }
+
+    //Inverse computation for the sake of optimistation
+    cln::cl_I inv = anemoi::pow(g, -n, p);
+    t = cln::cl_I_to_long(inv);
+
+    //Giant step
+    for (cln::cl_I i = 0; i < n; ++i) {
+        auto tmp = cln::mod(h * anemoi::pow(inv, i, p), p);
+
+        if (lookup[tmp] > 0) {
+            return lookup[tmp] - 1 + i * n;
+        }
+    }
+
+    //No solutions found
     return -1;
 }
 
